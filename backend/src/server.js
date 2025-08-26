1597f9445a1aedddb480d93eabd8a6252325e50b
Taewoo Park <taewoo9240@gmail.com>
2025-08-26 16:53:39 +0900

    feat(backend) : mongodb 환경 및 초기 연결 설정

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,8 +12,17 @@ app.get("/api/health", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log("✅ MONGODB Connected.");
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("❌ MONGODB Connection Failed.")
+    })
+
 
 module.exports = app;
